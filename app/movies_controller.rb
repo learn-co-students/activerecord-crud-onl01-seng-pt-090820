9f1babaa882dcfc00b26887b32b31a682f55418b
Carlj0666 <carlj1881@yahoo.com>
2020-11-19 11:53:27 -0800

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -20,18 +20,24 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new(attributes)
+  movie.save
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = nil)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
-  Movie.create do |m|
-    __
+  if args = nil
+    Movie.create do |m|
+      binding.pry
+      title = m.title
+      release_date = m.release_date
+
+    end
   end
 end
+
 
 def can_get_the_first_item_in_the_database
   __
