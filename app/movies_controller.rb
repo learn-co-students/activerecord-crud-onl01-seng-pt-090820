c5a961d8582683643f022690cff757ebdc5d2cb8
Carlj0666 <carlj1881@yahoo.com>
2020-11-20 15:07:06 -0800

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -39,19 +39,23 @@ end
 
 
 def can_get_the_first_item_in_the_database
-  binding.pry
+  first_film = Movie.first
+  first_film
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  last_film = Movie.last
+  last_film
 end
 
 def can_get_size_of_the_database
-  __
+  db_size = Movie.all.length
+  db_size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  first_id = Movie.find_by(id: 1)
+  first_id
 end
 
 def can_find_by_multiple_attributes
@@ -59,47 +63,51 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  movie = Movie.find_by(title: "Title", release_date: 2000, director: "Me")
+  movie
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  #binding.pry
+  movie_list = Movie.where("release_date > 2002").order('release_date DESC')
+  movie_list
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  awesome_flick = Movie.find_by(title: "Awesome Flick")
+  #binding.pry
+  awesome_flick.update(title: "Even Awesomer Flick")
+  awesome_flick.save
+  awesome_flick
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.create(title: "Wat?")
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
+    Movie.all.update(title: "A Movie")
   end
-  __
+
 end
 
 def can_destroy_a_single_item
-  Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  m = Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
+  m = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  m.destroy
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.destroy_all
 end
